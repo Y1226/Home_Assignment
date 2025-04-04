3e08787aebedcf890618e3fd6e2fef4efb914fa3
Y1226 <yepollack@gmail.com>
2025-04-04 16:37:42 +0300

    Final log_parser added.

--- xlsx_to_txt/xlsx_to_txt.py
+++ xlsx_to_txt/xlsx_to_txt.py
@@ -5,7 +5,7 @@ def xlsx_to_txt(input_file, output_file, delimiter="\t"):
     df = pd.read_excel(input_file, engine='openpyxl')
 
     # Convert DataFrame to a TXT file with a chosen delimiter (default: tab)
-    df.to_csv(output_file, sep=delimiter, index=False, header=False)
+    df.to_csv(output_file, sep=delimiter, index=False, header=True)
 
     print(f"Conversion complete: {output_file}")
 
